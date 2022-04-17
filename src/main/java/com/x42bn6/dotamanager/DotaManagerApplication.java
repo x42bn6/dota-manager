21e763ac8bc4570709e12f33e51c3f828fab5c96
x42bn6 <x42bn6@gmail.com>
2022-04-17 22:42:25 +0100

    Initial commit

---
+++ src/main/java/com/x42bn6/dotamanager/DotaManagerApplication.java
@@ -0,0 +1,13 @@
+package com.x42bn6.dotamanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotaManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotaManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/x42bn6/dotamanager/DotaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.x42bn6.dotamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotaManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
